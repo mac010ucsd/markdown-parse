07566bca0d577e6bc8b87531685e1a93aabd00f9
Mohammad Alkhalifah <56599163+Mkhlf@users.noreply.github.com>
2022-01-28 01:18:08 +0300

    add the test for the base case

---
+++ MarkdownParseTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    public String readFile(String file) throws IOException {
+        Path fileName = Path.of(file);
+        String contents = Files.readString(fileName);
+
+        return contents;
+    }
+
+    @Test
+    public void testBaseCase() throws IOException {
+        String contents = readFile("test-file.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks =
+                new ArrayList<>(List.of("https://something.com", "some-page.html"));
+
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
+
+    }
+}
