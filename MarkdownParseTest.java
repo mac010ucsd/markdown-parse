9e65580ee944bf42977fd92689dfadcb6cfdc74a
Matthew Chan <mchan11101@Matthews-MacBook-Air.local>
2022-01-27 22:07:56 -0800

    update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -33,9 +33,14 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testFile2() throws IOException {
+    public void testBackslashes() throws IOException {
         assertEquals(getLinksFromFile("test-backslash-escapes.md"), List.of("/close_bracket", "/single_)bracket", "/double_\\",
         "/triple_\\)bracket", "/quad_\\\\", "/open_(paren"));
+    }
+
+    @Test
+    public void testEmpty() throws IOException {
+        assertEquals(getLinksFromFile("test-empty.md"), List.of());
     }
     
 }
